7ec92d442f22dcb0b4dddb05e30eb6b7aba4ac98
tarikerenkurt <tarikerenkurt@gmail.com>
2025-05-12 16:54:35 +0200

    v0.0.6
    FIXED:: database connection fixed.

--- dev/connection.php
+++ dev/connection.php
@@ -1,14 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "lv_bookstore";
+$dbHost = "db";
+$dbName = "lv_bookstore";
+$dbUser = "root";
+$dbPassword = "root";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-?>
